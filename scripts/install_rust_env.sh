9b12355ec9bd9c3caf3b93047f7b9026aa795deb
salvivona <vivonasg@gmail.com>
2023-04-09 18:43:32 +0000

    ol

--- scripts/install_rust_env.sh
+++ scripts/install_rust_env.sh
@@ -10,14 +10,10 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 source "$HOME/.cargo/env"
 # Set Rust toolchain to 1.68.1
-# rustup default 1.68.1
-rustup install nightly-2023-01-21
-rustup default nightly
-rustup override set nightly
+rustup default 1.68.1
 
 # Add the wasm32-unknown-unknown target for 1.68.1
-rustup target add wasm32-unknown-unknown
-# rustup target add wasm32-unknown-unknown --toolchain 1.68.1
+rustup target add wasm32-unknown-unknown --toolchain 1.68.1
 
 echo "*** Initialized WASM build environment with Rust 1.68.1"
 
