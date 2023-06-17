739451f68e1e0edf0c4ed10d3ada9b83146c1ae3
Your Name <you@example.com>
2023-06-17 03:14:26 +0000

    bro

--- scripts/install_rust_env.sh
+++ scripts/install_rust_env.sh
@@ -7,16 +7,15 @@ set -e
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 export PATH="$HOME/.cargo/bin:$PATH"
-
 source "$HOME/.cargo/env"
-# Set Rust toolchain to 1.68.1
-rustup default 1.68.1
-
-rustup override set 1.68.1
-# Add the wasm32-unknown-unknown target for 1.68.1
-rustup target add wasm32-unknown-unknown --toolchain 1.68.1
-
 echo "*** Initialized WASM build environment with Rust 1.68.1"
 
+# Install cargo and Rust nightly
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
+apt-get install libclang-dev
+apt-get install protobuf-compiler
 
 
