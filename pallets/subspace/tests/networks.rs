44609e3d36c8460007e44eaf77e11052dc02ddac
Your Name <you@example.com>
2023-07-10 14:43:57 +0000

    added network tests

--- pallets/subspace/tests/networks.rs
+++ pallets/subspace/tests/networks.rs
@@ -22,6 +22,39 @@ fn test_add_network() {
 });}
 
 
+#[test]
+fn test_add_many_subnets() { 
+        new_test_ext().execute_with(|| {
+        for i in 0..100 {
+            add_network(i, U256::from(0));
+            assert_eq!(SubspaceModule::get_number_of_subnets(), i+1);
+        }
+});}
+
+
+
+#[test]
+fn test_set_max_allowed_uids() { 
+        new_test_ext().execute_with(|| {
+        let netuid = 0;
+        let stake = 1_000_000_000;
+        let max_uids = 100;
+        SubspaceModule::set_max_allowed_uids(netuid, max_uids);
+        for i in 0..max_uids {
+            register_module(netuid, U256::from(0), stake);
+            assert_eq!(SubspaceModule::get_subnet_n(netuid), i+1);
+        }
+        assert_eq!(SubspaceModule::get_subnet_n(netuid), max_uids);
+
+        for i in max_uids..max_uids+10 {
+            register_module(netuid, U256::from(0), stake);
+            assert_eq!(SubspaceModule::get_subnet_n(netuid), max_uids);
+        }
+
+        assert_eq!(SubspaceModule::get_subnet_n(netuid), max_uids);
+});}
+
+
 
 
 
