30a3a6b73e7916d4abb79a2a7a3af2563dee757d
Your Name <you@example.com>
2023-07-28 16:45:40 +0000

    specs and shi

--- scripts/install_rust_env.sh
+++ scripts/install_rust_env.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 # This script is meant to be run on Unix/Linux based systems
 set -e
@@ -7,18 +6,17 @@ set -e
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 export PATH="$HOME/.cargo/bin:$PATH"
-source "$HOME/.cargo/env"
+. "$HOME/.cargo/env"
 echo "*** Initialized WASM build environment with Rust 1.68.1"
 
 # Install cargo and Rust nightly
-rustup update
 rustup update nightly
-
 rustup install nightly-2023-01-01
 rustup override set nightly-2023-01-01
 rustup target add wasm32-unknown-unknown
 
-# apt-get install libclang-dev
-# apt-get install protobuf-compiler
-
-
+# Install dependencies
+apt-get update
+# Use the "yes" command to automatically provide 'Y' as the answer
+yes | apt-get install libclang-dev
+yes | apt-get install protobuf-compiler
