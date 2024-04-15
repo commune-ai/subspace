3bdeb36da7ea1f6051ffcf141e94f76833409245
YourUsername <functorflow69@users.noreply.github.com>
2024-04-15 09:48:20 +0000

    fix: migrations

--- pallets/subspace/src/migrations.rs
+++ pallets/subspace/src/migrations.rs
@@ -13,6 +13,7 @@ impl<T: Config> StorageInstance for Pallet<T> {
     const STORAGE_PREFIX: &'static str = "Subspace";
 }
 
+// Delegation update, migrations.
 pub mod v1 {
     use super::*;
 
@@ -38,13 +39,14 @@ pub mod v1 {
                 log::info!("Migrated DelegationFee to v1");
                 T::DbWeight::get().writes(1)
             } else {
-                log::info!("DelegationFee already updated");
+                log::info!("Storage v1 already updated");
                 Weight::zero()
             }
         }
     }
 }
 
+// Proposal update, migrations.
 pub mod v2 {
     use crate::voting::VoteMode;
 
@@ -92,39 +94,6 @@ pub mod v2 {
             if on_chain_version == 1 {
                 log::info!("Migrating to V2");
 
-                // Query for the threshold of stake that subnet needs to have
-                let subnet_stake_threshold = SubnetStakeThreshold::<T>::get();
-
-                // Here we will just use the lower bound of the burn
-                let old_burn = Pallet::<T>::global_params().min_burn;
-
-                // Iterate through all netuids and insert the old burn (minimum) value for each
-                // this is important as we don't want free registrations right after the runtime
-                // udpate
-                for netuid in N::<T>::iter_keys() {
-                    Burn::<T>::insert(netuid, old_burn);
-                    // update the emission that are below the threshold
-                    let emission_for_netuid =
-                        Pallet::<T>::calculate_network_emission(netuid, subnet_stake_threshold);
-                    // empty out the module emissin on that subnet, as their epoch won't run, we
-                    // don't want to confuse the user querying for the storage
-                    if emission_for_netuid == 0 {
-                        let name_count = Name::<T>::iter_prefix(netuid).count();
-                        let new_emission = vec![0; name_count];
-                        Emission::<T>::insert(netuid, new_emission);
-                    }
-                    SubnetEmission::<T>::insert(netuid, emission_for_netuid);
-
-                    Active::<T>::mutate(netuid, |v| v.push(true));
-                    Consensus::<T>::mutate(netuid, |v| v.push(0));
-                    PruningScores::<T>::mutate(netuid, |v| v.push(0));
-                    Rank::<T>::mutate(netuid, |v| v.push(0));
-                    Trust::<T>::mutate(netuid, |v| v.push(0));
-                    ValidatorPermits::<T>::mutate(netuid, |v| v.push(false));
-                    ValidatorTrust::<T>::mutate(netuid, |v| v.push(0));
-                }
-                log::info!("Migrated Burn");
-
                 // Migrate StakeFrom storage
                 for (netuid, module_key, stake_from) in old_storage::StakeFrom::<T>::iter() {
                     let new_stake_from: BTreeMap<T::AccountId, u64> =
@@ -156,10 +125,66 @@ pub mod v2 {
                 // Update the storage version to 2
                 StorageVersion::new(2).put::<Pallet<T>>();
 
-                log::info!("Migrated v2");
+                log::info!("Migrated to v2");
                 T::DbWeight::get().reads_writes(1, 1)
             } else {
-                log::info!("Subnets already updated");
+                log::info!("Storage v2 already updated");
+                Weight::zero()
+            }
+        }
+    }
+}
+
+// Incentives update, migrations.
+pub mod v3 {
+    use super::*;
+
+    pub struct MigrateToV3<T>(sp_std::marker::PhantomData<T>);
+
+    impl<T: Config> OnRuntimeUpgrade for MigrateToV3<T> {
+        fn on_runtime_upgrade() -> Weight {
+            let on_chain_version = StorageVersion::get::<Pallet<T>>();
+
+            // Migrate Burn to v3
+            if on_chain_version == 2 {
+                // Query for the threshold of stake that subnet needs to have
+                let subnet_stake_threshold = SubnetStakeThreshold::<T>::get();
+                // Here we will just use the lower bound of the burn
+                let old_burn = Pallet::<T>::global_params().min_burn;
+
+                // Find the highest netuid from the subnetnames
+                let largest_netuid = SubnetNames::<T>::iter_keys().max().unwrap_or(0);
+                // Iterate through all netuids and insert the old burn (minimum) value for each
+                // this is important as we don't want free registrations right after the runtime
+                // udpate
+                for netuid in 0..=largest_netuid {
+                    Burn::<T>::insert(netuid, old_burn);
+                    // update the emission that are below the threshold
+                    let emission_for_netuid =
+                        Pallet::<T>::calculate_network_emission(netuid, subnet_stake_threshold);
+                    // empty out the module emissin on that subnet, as their epoch won't run, we
+                    // don't want to confuse the user querying for the storage
+                    if emission_for_netuid == 0 {
+                        let name_count = Name::<T>::iter_prefix(netuid).count();
+                        let new_emission = vec![0; name_count];
+                        Emission::<T>::insert(netuid, new_emission);
+                    }
+                    SubnetEmission::<T>::insert(netuid, emission_for_netuid);
+
+                    Active::<T>::mutate(netuid, |v| v.push(true));
+                    Consensus::<T>::mutate(netuid, |v| v.push(0));
+                    PruningScores::<T>::mutate(netuid, |v| v.push(0));
+                    Rank::<T>::mutate(netuid, |v| v.push(0));
+                    Trust::<T>::mutate(netuid, |v| v.push(0));
+                    ValidatorPermits::<T>::mutate(netuid, |v| v.push(false));
+                    ValidatorTrust::<T>::mutate(netuid, |v| v.push(0));
+                }
+
+                StorageVersion::new(3).put::<Pallet<T>>();
+                log::info!("Migrated subnets to v3");
+                T::DbWeight::get().writes(largest_netuid as u64)
+            } else {
+                log::info!("Storage v3 already updated");
                 Weight::zero()
             }
         }
