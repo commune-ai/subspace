0a04cd988776c4dab2439e1da110e948a1044818
YourUsername <functorflow69@users.noreply.github.com>
2024-04-16 21:47:19 +0000

    ignoring test

--- pallets/subspace/tests/step_yuma.rs
+++ pallets/subspace/tests/step_yuma.rs
@@ -169,6 +169,7 @@ fn test_10_graph() {
 
 // Testing weight expiration, on subnets running yuma
 #[test]
+#[ignore]
 fn test_yuma_weight_expiration() {
     new_test_ext().execute_with(|| {
         todo!();
