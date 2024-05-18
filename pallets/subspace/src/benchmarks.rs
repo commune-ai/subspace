a8ef9da6742725c55ccc69e1fa4e7b2ed4255178
YourUsername <zimulajan0@gmail.com>
2024-05-18 21:20:31 +0000

    wip working weight info

--- pallets/subspace/src/benchmarks.rs
+++ pallets/subspace/src/benchmarks.rs
@@ -35,19 +35,11 @@ fn register_mock<T: Config>(
 const REMOVE_WHEN_STAKING: u64 = 500;
 
 benchmarks! {
+    // ---------------------------------
+    // Consensus operations
+    // ---------------------------------
 
-
-    register {
-        log::info!("Running register benchmark");
-        let key: T::AccountId = account("Alice", 0, 1);
-        let module_key: T::AccountId = account("ModuleKey", 0, 2);
-        let stake = 100000000000000u64;
-        SubspaceMod::<T>::add_balance_to_account(
-            &key,
-            SubspaceMod::<T>::u64_to_balance(stake + 2000).unwrap(),
-        );
-    }: register(RawOrigin::Signed(key.clone()), "test".as_bytes().to_vec(), "test".as_bytes().to_vec(), "test".as_bytes().to_vec(), stake.into(), module_key.clone(), Some("metadata".as_bytes().to_vec()))
-
+    // 0
     set_weights {
         log::info!("Running set_weights benchmark");
         let netuid = 0;
@@ -60,6 +52,11 @@ benchmarks! {
         let weights = vec![10];
     }: set_weights(RawOrigin::Signed(module_key2), netuid, uids, weights)
 
+    // ---------------------------------
+    // Stake operations
+    // ---------------------------------
+
+    // 1
     add_stake {
         log::info!("Running add_stake benchmark");
         let key: T::AccountId = account("Alice", 0, 1);
@@ -73,6 +70,27 @@ benchmarks! {
         register_mock::<T>(module_key.clone(), module_key.clone(), stake.clone(), "test".as_bytes().to_vec())?;
     }: add_stake(RawOrigin::Signed(key), netuid, module_key, stake)
 
+    // 2
+    remove_stake {
+        log::info!("Running remove_stake benchmark");
+        let caller: T::AccountId = account("Alice", 0, 1);
+        let netuid = 0;
+        let module_key: T::AccountId = account("ModuleKey", 0, 2);
+        let stake = 100000000000000u64;
+        register_mock::<T>(module_key.clone(), module_key.clone(), stake, "test".as_bytes().to_vec())?;
+        let amount = 100000;
+        SubspaceMod::<T>::add_balance_to_account(
+            &caller,
+            SubspaceMod::<T>::u64_to_balance(amount).unwrap(),
+        );
+        SubspaceMod::<T>::add_stake(RawOrigin::Signed(caller.clone()).into(), netuid, module_key.clone(), amount - REMOVE_WHEN_STAKING)?;
+    }: remove_stake(RawOrigin::Signed(caller), netuid, module_key, amount - REMOVE_WHEN_STAKING)
+
+    // ---------------------------------
+    // Bulk stake operations
+    // ---------------------------------
+
+    // 3
     add_stake_multiple {
         log::info!("Running add_stake_multiple benchmark");
         let caller: T::AccountId = account("Alice", 0, 1);
@@ -92,21 +110,7 @@ benchmarks! {
         amounts.iter_mut().for_each(|x| *x -= REMOVE_WHEN_STAKING);
     }: add_stake_multiple(RawOrigin::Signed(caller), netuid, module_keys, amounts)
 
-    remove_stake {
-        log::info!("Running remove_stake benchmark");
-        let caller: T::AccountId = account("Alice", 0, 1);
-        let netuid = 0;
-        let module_key: T::AccountId = account("ModuleKey", 0, 2);
-        let stake = 100000000000000u64;
-        register_mock::<T>(module_key.clone(), module_key.clone(), stake, "test".as_bytes().to_vec())?;
-        let amount = 100000;
-        SubspaceMod::<T>::add_balance_to_account(
-            &caller,
-            SubspaceMod::<T>::u64_to_balance(amount).unwrap(),
-        );
-        SubspaceMod::<T>::add_stake(RawOrigin::Signed(caller.clone()).into(), netuid, module_key.clone(), amount - REMOVE_WHEN_STAKING)?;
-    }: remove_stake(RawOrigin::Signed(caller), netuid, module_key, amount - REMOVE_WHEN_STAKING)
-
+    // 4
     remove_stake_multiple {
         log::info!("Running remove_stake_multiple benchmark");
         let caller: T::AccountId = account("Alice", 0, 1);
@@ -127,4 +131,94 @@ benchmarks! {
         SubspaceMod::<T>::add_stake_multiple(RawOrigin::Signed(caller.clone()).into(), netuid, module_keys.clone(), amounts.clone())?;
     }: remove_stake_multiple(RawOrigin::Signed(caller), netuid, module_keys, amounts)
 
+    // ---------------------------------
+    // Transfers
+    // ---------------------------------
+
+    // 5
+    transfer_stake {
+        log::info!("Running transfer_stake benchmark");
+        let caller: T::AccountId = account("Alice", 0, 1);
+        let netuid = 0;
+        let module_key: T::AccountId = account("ModuleKey", 0, 2);
+        let new_module_key: T::AccountId = account("NewModuleKey", 0, 3);
+        let stake = 100000000000000u64;
+        register_mock::<T>(module_key.clone(), module_key.clone(), stake, "test".as_bytes().to_vec())?;
+        register_mock::<T>(new_module_key.clone(), new_module_key.clone(), stake, "test1".as_bytes().to_vec())?;
+        let amount = 10000;
+        SubspaceMod::<T>::add_balance_to_account(
+            &caller,
+            SubspaceMod::<T>::u64_to_balance(amount).unwrap(),
+        );
+        SubspaceMod::<T>::add_stake(RawOrigin::Signed(caller.clone()).into(), netuid, module_key.clone(), amount - REMOVE_WHEN_STAKING)?;
+    }: transfer_stake(RawOrigin::Signed(caller), netuid, module_key, new_module_key, amount - REMOVE_WHEN_STAKING)
+
+    // 6
+    transfer_multiple {
+        log::info!("Running transfer_multiple benchmark");
+        let caller: T::AccountId = account("Alice", 0, 1);
+        let dest1: T::AccountId = account("Dest1", 0, 2);
+        let dest2: T::AccountId = account("Dest2", 0, 3);
+        let destinations = vec![dest1.clone(), dest2.clone()];
+        let mut amounts = vec![10000, 20000];
+        SubspaceMod::<T>::add_balance_to_account(
+            &caller,
+            SubspaceMod::<T>::u64_to_balance(amounts.iter().sum()).unwrap(),
+        );
+        // Reduce by REMOVE_WHEN_STAKING
+        amounts.iter_mut().for_each(|x| *x -= REMOVE_WHEN_STAKING);
+
+    }: transfer_multiple(RawOrigin::Signed(caller), destinations, amounts)
+
+    // ---------------------------------
+    // Registereing / Deregistering
+    // ---------------------------------
+
+    // 7
+    register {
+        log::info!("Running register benchmark");
+        let key: T::AccountId = account("Alice", 0, 1);
+        let module_key: T::AccountId = account("ModuleKey", 0, 2);
+        let stake = 100000000000000u64;
+        SubspaceMod::<T>::add_balance_to_account(
+            &key,
+            SubspaceMod::<T>::u64_to_balance(stake + 2000).unwrap(),
+        );
+    }: register(RawOrigin::Signed(key.clone()), "test".as_bytes().to_vec(), "test".as_bytes().to_vec(), "test".as_bytes().to_vec(), stake.into(), module_key.clone(), Some("metadata".as_bytes().to_vec()))
+
+    // 8
+    deregister {
+        log::info!("Running deregister benchmark");
+        let caller: T::AccountId = account("Alice", 0, 1);
+        let netuid = 0;
+        let stake = 100000000000000u64;
+        register_mock::<T>(caller.clone(), caller.clone(), stake, "test".as_bytes().to_vec())?;
+    }: deregister(RawOrigin::Signed(caller), netuid)
+
+    // ---------------------------------
+    // Updating
+    // ---------------------------------
+
+    // ---------------------------------
+    // Subnet 0 DAO
+    // ---------------------------------
+
+    // ---------------------------------
+    // Adding proposals
+    // ---------------------------------
+
+    // ---------------------------------
+    // Voting / Unvoting proposals
+    // ---------------------------------
+
+    // ---------------------------------
+    // Profit sharing
+    // ---------------------------------
+
+    // ---------------------------------
+    // Testnet
+    // ---------------------------------
+
+    // 21
+    // TODO: Add testnet benchmarks later
 }
