09b5406a81e8a6b5f2373720c92a4890ce02919b
João Victor <65056371+devwckd@users.noreply.github.com>
2024-05-20 23:46:07 -0300

    refac(pallets/governance): move voting from subspace (#108)

--- pallets/governance/src/lib.rs
+++ pallets/governance/src/lib.rs
@@ -2,20 +2,31 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod migrations;
+pub mod proposal;
+pub mod voting;
+
+use core::marker::PhantomData;
+
 use frame_support::{
-    dispatch::DispatchResult, ensure, sp_runtime::Percent, storage::with_storage_layer,
-    traits::ConstU32, BoundedBTreeSet, BoundedVec, DebugNoBound,
+    ensure,
+    sp_runtime::{DispatchError, Percent},
 };
-pub use pallet::*;
+use frame_system::pallet_prelude::OriginFor;
+use pallet_subspace::voting::VoteMode;
 use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
+use proposal::{Proposal, ProposalId};
 use scale_info::TypeInfo;
+use sp_std::vec::Vec;
+
+pub use pallet::*;
 
 type SubnetId = u16;
-type ProposalId = u64;
 type Nanos = u64;
 
 #[frame_support::pallet]
 pub mod pallet {
+    #![allow(clippy::too_many_arguments)]
+
     use frame_support::{
         pallet_prelude::{ValueQuery, *},
         traits::Currency,
@@ -49,24 +60,161 @@ pub mod pallet {
                 block_number.try_into().ok().expect("blockchain won't pass 2 ^ 64 blocks");
 
             if block_number % 100 == 0 {
-                tick_proposals::<T>(block_number);
+                proposal::tick_proposals::<T>(block_number);
             }
 
             Weight::zero()
         }
     }
 
-    /// A map of all proposals, indexed by their IDs.
     #[pallet::storage]
-    pub(crate) type Proposals<T: Config> = StorageMap<_, Identity, ProposalId, Proposal<T>>;
+    pub type GovernanceConfig<T: Config> = StorageValue<_, GovernanceConfiguration<T>, ValueQuery>;
 
     #[pallet::storage]
-    pub(crate) type GlobalProposalCost<T: Config> =
-        StorageValue<_, Nanos, ValueQuery, T::DefaultProposalCost>;
+    pub type SubnetGovernanceConfig<T: Config> =
+        StorageMap<_, Identity, SubnetId, GovernanceConfiguration<T>, ValueQuery>;
 
     #[pallet::storage]
-    pub(crate) type SubnetsProposalCosts<T: Config> =
-        StorageMap<_, Identity, SubnetId, Nanos, ValueQuery, T::DefaultProposalCost>;
+    pub type Proposals<T: Config> = StorageMap<_, Identity, ProposalId, Proposal<T>>;
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn add_global_proposal(
+            origin: OriginFor<T>,
+            data: Vec<u8>,
+            max_name_length: u16,
+            min_name_length: u16,
+            max_allowed_subnets: u16,
+            max_allowed_modules: u16,
+            max_registrations_per_block: u16,
+            max_allowed_weights: u16,
+            max_burn: u64,
+            min_burn: u64,
+            floor_delegation_fee: Percent,
+            floor_founder_share: u8,
+            min_weight_stake: u64,
+            target_registrations_per_interval: u16,
+            target_registrations_interval: u16,
+            adjustment_alpha: u64,
+            curator: T::AccountId,
+            subnet_stake_threshold: Percent,
+            proposal_cost: u64,
+            proposal_expiration: u32,
+            proposal_participation_threshold: Percent,
+            general_subnet_application_cost: u64,
+        ) -> DispatchResult {
+            let mut params = pallet_subspace::Pallet::<T>::global_params();
+            params.max_name_length = max_name_length;
+            params.min_name_length = min_name_length;
+            params.max_allowed_subnets = max_allowed_subnets;
+            params.max_allowed_modules = max_allowed_modules;
+            params.max_registrations_per_block = max_registrations_per_block;
+            params.max_allowed_weights = max_allowed_weights;
+            params.floor_delegation_fee = floor_delegation_fee;
+            params.floor_founder_share = floor_founder_share;
+            params.min_weight_stake = min_weight_stake;
+            params.curator = curator;
+            params.subnet_stake_threshold = subnet_stake_threshold;
+            params.proposal_cost = proposal_cost;
+            params.proposal_expiration = proposal_expiration;
+            params.proposal_participation_threshold = proposal_participation_threshold;
+            params.general_subnet_application_cost = general_subnet_application_cost;
+
+            params.burn_config.min_burn = min_burn;
+            params.burn_config.max_burn = max_burn;
+            params.burn_config.adjustment_alpha = adjustment_alpha;
+            params.burn_config.adjustment_interval = target_registrations_interval;
+            params.burn_config.expected_registrations = target_registrations_per_interval;
+
+            Self::do_add_global_proposal(origin, data, params)
+        }
+
+        #[pallet::call_index(1)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn add_subnet_proposal(
+            origin: OriginFor<T>,
+            subnet_id: u16,
+            data: Vec<u8>,
+            founder: T::AccountId,
+            name: BoundedVec<u8, ConstU32<256>>,
+            founder_share: u16,
+            immunity_period: u16,
+            incentive_ratio: u16,
+            max_allowed_uids: u16,
+            max_allowed_weights: u16,
+            min_allowed_weights: u16,
+            min_stake: u64,
+            max_weight_age: u64,
+            tempo: u16,
+            trust_ratio: u16,
+            maximum_set_weight_calls_per_epoch: u16,
+            vote_mode: VoteMode,
+            bonds_ma: u64,
+        ) -> DispatchResult {
+            let mut params = pallet_subspace::Pallet::subnet_params(subnet_id);
+            params.founder = founder;
+            params.name = name;
+            params.founder_share = founder_share;
+            params.immunity_period = immunity_period;
+            params.incentive_ratio = incentive_ratio;
+            params.max_allowed_uids = max_allowed_uids;
+            params.max_allowed_weights = max_allowed_weights;
+            params.min_allowed_weights = min_allowed_weights;
+            params.min_stake = min_stake;
+            params.max_weight_age = max_weight_age;
+            params.tempo = tempo;
+            params.trust_ratio = trust_ratio;
+            params.maximum_set_weight_calls_per_epoch = maximum_set_weight_calls_per_epoch;
+            params.vote_mode = vote_mode;
+            params.bonds_ma = bonds_ma;
+            Self::do_add_subnet_proposal(origin, subnet_id, data, params)
+        }
+
+        #[pallet::call_index(2)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn add_custom_proposal(origin: OriginFor<T>, data: Vec<u8>) -> DispatchResult {
+            Self::do_add_custom_proposal(origin, data)
+        }
+
+        #[pallet::call_index(3)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn add_custom_subnet_proposal(
+            origin: OriginFor<T>,
+            netuid: u16,
+            data: Vec<u8>,
+        ) -> DispatchResult {
+            Self::do_add_custom_subnet_proposal(origin, netuid, data)
+        }
+
+        #[pallet::call_index(4)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn add_transfer_dao_treasury_proposal(
+            origin: OriginFor<T>,
+            data: Vec<u8>,
+            value: u64,
+            dest: T::AccountId,
+        ) -> DispatchResult {
+            Self::do_add_transfer_dao_treasury_proposal(origin, data, value, dest)
+        }
+
+        #[pallet::call_index(5)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn vote_proposal(
+            origin: OriginFor<T>,
+            proposal_id: u64,
+            agree: bool,
+        ) -> DispatchResult {
+            Self::do_vote_proposal(origin, proposal_id, agree)
+        }
+
+        #[pallet::call_index(6)]
+        #[pallet::weight((Weight::zero(), DispatchClass::Normal, Pays::No))]
+        pub fn remove_vote_proposal(origin: OriginFor<T>, proposal_id: u64) -> DispatchResult {
+            Self::do_remove_vote_proposal(origin, proposal_id)
+        }
+    }
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(crate) fn deposit_event)]
@@ -76,6 +224,9 @@ pub mod pallet {
         ProposalAccepted(ProposalId),
         ProposalRefused(ProposalId),
         ProposalExpired(ProposalId),
+
+        ProposalVoted(u64, T::AccountId, bool),
+        ProposalVoteUnregistered(u64, T::AccountId),
     }
 
     #[pallet::error]
@@ -84,227 +235,72 @@ pub mod pallet {
         ProposalIsFinished,
         /// Invalid parameters were provided to the finalization process.
         InvalidProposalFinalizationParameters,
+        /// Invalid parameters were provided to the voting process.
+        InvalidProposalVotingParameters,
+        /// Negative proposal cost when setting global or subnet governance configuration
+        InvalidProposalCost,
+        /// Negative expiration when setting global or subnet governance configuration
+        InvalidExpiration,
+        /// Key doesn't have enough tokens to create a proposal
+        NotEnoughBalanceToPropose,
+        /// Proposal data is empty
+        ProposalDataTooSmall,
+        /// Proposal data is bigger than 256 characters
+        ProposalDataTooLarge,
+        /// Proposal with given id doesn't exist
+        ProposalNotFound,
+        /// Proposal was either accepted, refused or expired and cannot accept votes
+        ProposalClosed,
+        /// Proposal data isn't composed by valid UTF-8 characters
+        InvalidProposalData,
+        /// Invalid value given when transforming a u64 into T::Currency
+        InvalidCurrencyConversionValue,
+        /// Dao Treasury doesn't have enough funds to be transferred
+        InsufficientDaoTreasuryFunds,
+        /// Subnet is on Authority Mode
+        NotVoteMode,
+        /// Key has already voted on given Proposal
+        AlreadyVoted,
+        /// Key hasn't voted on given Proposal
+        NotVoted,
+        /// Key doesn't have enough stake to vote
+        InsufficientStake,
     }
 }
 
-#[derive(DebugNoBound, TypeInfo, Decode, Encode, MaxEncodedLen)]
+#[derive(
+    Clone, TypeInfo, Decode, Encode, PartialEq, Eq, frame_support::DebugNoBound, MaxEncodedLen,
+)]
 #[scale_info(skip_type_params(T))]
-pub struct Proposal<T: Config> {
-    pub id: ProposalId,
-    pub proposer: T::AccountId,
-    pub expiration_block: u64,
-    pub data: ProposalData<T>,
-    pub status: ProposalStatus<T>,
-    pub metadata: BoundedVec<u8, ConstU32<256>>,
-    pub proposal_cost: u64,
-    pub creation_block: u64,
+pub struct GovernanceConfiguration<T: Config> {
+    pub proposal_cost: Nanos,
+    pub expiration: u32,
+    pub vote_mode: VoteMode,
+    pub _pd: PhantomData<T>,
 }
 
-impl<T: Config> Proposal<T> {
-    /// Whether the proposal is still active.
-    #[must_use]
-    pub fn is_active(&self) -> bool {
-        matches!(self.status, ProposalStatus::Open { .. })
-    }
-
-    /// Returns the subnet ID that this proposal impact.s
-    #[must_use]
-    pub fn subnet_id(&self) -> Option<u16> {
-        match &self.data {
-            ProposalData::SubnetParams { subnet_id, .. }
-            | ProposalData::SubnetCustom { subnet_id, .. } => Some(*subnet_id),
-            _ => None,
+impl<T: Config> Default for GovernanceConfiguration<T> {
+    fn default() -> Self {
+        Self {
+            proposal_cost: 10_000_000_000_000,
+            expiration: 130000,
+            vote_mode: VoteMode::Vote,
+            _pd: PhantomData,
         }
     }
+}
 
-    /// Marks a proposal as accepted and overrides the storage value.
-    pub fn accept(mut self, block_number: u64) -> DispatchResult {
-        ensure!(self.is_active(), Error::<T>::ProposalIsFinished);
+impl<T: Config> GovernanceConfiguration<T> {
+    pub fn apply_global(self) -> Result<(), DispatchError> {
+        ensure!(self.proposal_cost > 0, Error::<T>::InvalidProposalCost);
+        ensure!(self.expiration > 0, Error::<T>::InvalidProposalCost);
 
-        self.status = ProposalStatus::Accepted {
-            block: block_number,
-            stake_for: 0,
-            stake_against: 0,
-        };
-
-        Proposals::<T>::insert(self.id, &self);
-        Pallet::<T>::deposit_event(Event::ProposalAccepted(self.id));
-
-        execute_proposal(self)?;
-
+        GovernanceConfig::<T>::set(self);
         Ok(())
     }
 
-    /// Marks a proposal as refused and overrides the storage value.
-    pub fn refuse(mut self, block_number: u64) -> DispatchResult {
-        ensure!(self.is_active(), Error::<T>::ProposalIsFinished);
-
-        self.status = ProposalStatus::Refused {
-            block: block_number,
-            stake_for: 0,
-            stake_against: 0,
-        };
-
-        Proposals::<T>::insert(self.id, &self);
-        Pallet::<T>::deposit_event(Event::ProposalRefused(self.id));
-
+    pub fn apply_subnet(self, subnet_id: SubnetId) -> Result<(), DispatchError> {
+        SubnetGovernanceConfig::<T>::set(subnet_id, self);
         Ok(())
     }
-
-    /// Marks a proposal as expired and overrides the storage value.
-    pub fn expire(mut self, block_number: u64) -> DispatchResult {
-        ensure!(self.is_active(), Error::<T>::ProposalIsFinished);
-        ensure!(
-            block_number >= self.expiration_block,
-            Error::<T>::InvalidProposalFinalizationParameters
-        );
-
-        self.status = ProposalStatus::Expired;
-
-        Proposals::<T>::insert(self.id, &self);
-        Pallet::<T>::deposit_event(Event::ProposalExpired(self.id));
-
-        Ok(())
-    }
-}
-
-#[derive(Clone, DebugNoBound, TypeInfo, Decode, Encode, MaxEncodedLen)]
-#[scale_info(skip_type_params(T))]
-pub enum ProposalStatus<T: Config> {
-    Open {
-        votes_for: BoundedBTreeSet<T::AccountId, ConstU32<{ u32::MAX }>>,
-        votes_against: BoundedBTreeSet<T::AccountId, ConstU32<{ u32::MAX }>>,
-    },
-    Accepted {
-        block: u64,
-        stake_for: u64,
-        stake_against: u64,
-    },
-    Refused {
-        block: u64,
-        stake_for: u64,
-        stake_against: u64,
-    },
-    Expired,
-}
-
-#[derive(DebugNoBound, TypeInfo, Decode, Encode, MaxEncodedLen)]
-#[scale_info(skip_type_params(T))]
-pub enum ProposalData<T: Config> {
-    GlobalCustom,
-    GlobalParams(pallet_subspace::GlobalParams<T>),
-    SubnetCustom {
-        subnet_id: SubnetId,
-    },
-    SubnetParams {
-        subnet_id: SubnetId,
-        params: pallet_subspace::SubnetParams<T>,
-    },
-    TransferDaoTreasury {
-        account: T::AccountId,
-        amount: u64,
-    },
-}
-
-impl<T: Config> ProposalData<T> {
-    /// The required amount of stake each of the proposal types requires in order to pass.
-    #[must_use]
-    pub fn required_stake(&self) -> Percent {
-        match self {
-            Self::GlobalCustom | Self::SubnetCustom { .. } | Self::TransferDaoTreasury { .. } => {
-                Percent::from_parts(50)
-            }
-            Self::GlobalParams(_) | Self::SubnetParams { .. } => Percent::from_parts(40),
-        }
-    }
-}
-
-fn tick_proposals<T: Config>(block_number: u64) {
-    for (id, proposal) in Proposals::<T>::iter().filter(|(_, p)| p.is_active()) {
-        let res = with_storage_layer(|| tick_proposal(block_number, proposal));
-        if let Err(err) = res {
-            log::error!("failed to tick proposal {id}: {err:?}, skipping...");
-        }
-    }
-}
-
-fn tick_proposal<T: Config>(block_number: u64, proposal: Proposal<T>) -> DispatchResult {
-    use pallet_subspace::Pallet as SubspacePallet;
-
-    let subnet_id = proposal.subnet_id();
-
-    let ProposalStatus::Open {
-        votes_for,
-        votes_against,
-    } = &proposal.status
-    else {
-        return Err(Error::<T>::ProposalIsFinished.into());
-    };
-
-    let votes_for: u64 = votes_for
-        .iter()
-        .map(|id| SubspacePallet::<T>::get_account_stake(id, subnet_id))
-        .sum();
-    let votes_against: u64 = votes_against
-        .iter()
-        .map(|id| SubspacePallet::<T>::get_account_stake(id, subnet_id))
-        .sum();
-
-    let total_stake = votes_for + votes_against;
-    let minimal_stake_to_execute =
-        SubspacePallet::<T>::get_minimal_stake_to_execute_with_percentage(
-            proposal.data.required_stake(),
-            subnet_id,
-        );
-
-    if total_stake >= minimal_stake_to_execute {
-        if votes_against > votes_for {
-            proposal.refuse(block_number)
-        } else {
-            proposal.accept(block_number)
-        }
-    } else if block_number >= proposal.expiration_block {
-        proposal.expire(block_number)
-    } else {
-        Ok(())
-    }
-}
-
-fn execute_proposal<T: Config>(proposal: Proposal<T>) -> DispatchResult {
-    use pallet_subspace::{
-        subnet::SubnetChangeset, DaoTreasuryAddress, Event as SubspaceEvent,
-        Pallet as SubspacePallet,
-    };
-
-    match &proposal.data {
-        ProposalData::GlobalCustom | ProposalData::SubnetCustom { .. } => {
-            // No specific action needed for custom proposals
-            // The owners will handle the off-chain logic
-        }
-        ProposalData::GlobalParams(params) => {
-            SubspacePallet::<T>::set_global_params(params.clone());
-            SubspacePallet::<T>::deposit_event(SubspaceEvent::GlobalParamsUpdated(params.clone()));
-        }
-        ProposalData::SubnetParams { subnet_id, params } => {
-            let changeset = SubnetChangeset::<T>::update(*subnet_id, params.clone())?;
-            changeset.apply(*subnet_id)?;
-            SubspacePallet::<T>::deposit_event(SubspaceEvent::SubnetParamsUpdated(*subnet_id));
-        }
-        ProposalData::TransferDaoTreasury { account, amount } => {
-            SubspacePallet::<T>::transfer_balance_to_account(
-                // ! Make sure transfer balance is using safe transfer (keep alive)
-                &DaoTreasuryAddress::<T>::get(),
-                account,
-                *amount,
-            )?;
-        }
-    }
-
-    // Give the proposer back his tokens, if the proposal passed
-    SubspacePallet::<T>::add_balance_to_account(
-        &proposal.proposer,
-        SubspacePallet::<T>::u64_to_balance(proposal.proposal_cost).unwrap(),
-    );
-
-    Ok(())
 }
