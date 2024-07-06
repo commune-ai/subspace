bcb40898fcb77aad79f3a96f52be85abaed55384
supreme <zimulajan0@gmail.com>
2024-07-06 18:41:27 +0200

    feat: adding snapshot builder

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     naersk.url = "github:nix-community/naersk";
+    mach-nix.url = "github:DavHau/mach-nix/3.5.0";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, naersk, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk, mach-nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -17,26 +18,39 @@
         };
         rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        generalBuildInputs = with pkgs; [pkg-config rocksdb zstd.dev bashInteractive];
+        generalBuildInputs = with pkgs; [ pkg-config rocksdb zstd.dev bashInteractive ];
         buildInputs = with pkgs;
           if pkgs.stdenv.isLinux
-          then generalBuildInputs ++ [jemalloc]
+          then generalBuildInputs ++ [ jemalloc ]
           else generalBuildInputs;
-        nativeBuildInputs = with pkgs; [git rust clang protobuf];
+        nativeBuildInputs = with pkgs; [ git rust clang protobuf ];
 
         naersk' = pkgs.callPackage naersk {
           cargo = rust;
           rustc = rust;
         };
-      in {
+
+        pythonEnv = mach-nix.lib.${system}.mkPython {
+          requirements = ''
+            substrate-interface
+            setuptools
+          '';
+        };
+      in
+      {
         devShells.default = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
+          buildInputs = buildInputs ++ [ pythonEnv ];
+          nativeBuildInputs = nativeBuildInputs;
 
           env = {
             LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
             ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
             ZSTD_SYS_USE_PKG_CONFIG = "true";
           } // nixpkgs.lib.optionalAttrs pkgs.stdenv.isLinux { JEMALLOC_OVERRIDE = "${pkgs.jemalloc}/lib/libjemalloc.so"; };
+
+          shellHook = ''
+            export PATH=${pythonEnv}/bin:$PATH
+          '';
         };
 
         packages.default = naersk'.buildPackage {
