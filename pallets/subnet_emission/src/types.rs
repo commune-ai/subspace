cc48e3ffbf65c2498cbc99dc761506619f6652f0
devwckd <dev.wckd@gmail.com>
2024-10-21 17:22:18 -0300

    chore: isolate complex type into  definitions

--- pallets/subnet_emission/src/types.rs
+++ pallets/subnet_emission/src/types.rs
@@ -2,6 +2,7 @@ use super::*;
 
 pub type PublicKey = (Vec<u8>, Vec<u8>);
 pub type BlockWeights = (u64, Vec<(u16, Vec<(u16, u16)>, Vec<u8>)>);
+pub type KeylessBlockWeights = (u64, Vec<(u16, Vec<(u16, u16)>)>);
 
 #[derive(Clone, Encode, Decode, TypeInfo)]
 pub struct DecryptionNodeInfo<T: Config> {
