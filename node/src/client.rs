09cb1df1cc0b47bc460f73f02913548e55d91011
Honza <115138297+Supremesource@users.noreply.github.com>
2024-10-24 21:29:11 +0200

    fix/eth (#234)
    * fix: client
    
    * FUCK FRONTIER BLOCK IMPORT
    
    * fix clippy

--- node/src/client.rs
+++ node/src/client.rs
@@ -1,10 +1,58 @@
-use node_subspace_runtime::opaque::Block;
+use crate::eth::EthCompatRuntimeApiCollection;
+use node_subspace_runtime::{opaque::Block, AccountId, Balance, Nonce};
 use sc_executor::WasmExecutor;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 
 /// Full backend.
 pub type FullBackend = sc_service::TFullBackend<Block>;
-pub type WasmClient =
-    sc_service::TFullClient<Block, node_subspace_runtime::RuntimeApi, WasmExecutor<HostFunctions>>;
+pub type WasmClient<RuntimeApi> =
+    sc_service::TFullClient<Block, RuntimeApi, WasmExecutor<HostFunctions>>;
+
+pub type Client = WasmClient<node_subspace_runtime::RuntimeApi>;
+
+/// A set of APIs that every runtimes must implement.
+pub trait BaseRuntimeApiCollection:
+    sp_api::ApiExt<Block>
+    + sp_api::Metadata<Block>
+    + sp_block_builder::BlockBuilder<Block>
+    + sp_offchain::OffchainWorkerApi<Block>
+    + sp_session::SessionKeys<Block>
+    + sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
+{
+}
+
+impl<Api> BaseRuntimeApiCollection for Api where
+    Api: sp_api::ApiExt<Block>
+        + sp_api::Metadata<Block>
+        + sp_block_builder::BlockBuilder<Block>
+        + sp_offchain::OffchainWorkerApi<Block>
+        + sp_session::SessionKeys<Block>
+        + sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
+{
+}
+
+/// A set of APIs that template runtime must implement.
+pub trait RuntimeApiCollection:
+    BaseRuntimeApiCollection
+    + EthCompatRuntimeApiCollection
+    + sp_consensus_aura::AuraApi<Block, AuraId>
+    + sp_consensus_grandpa::GrandpaApi<Block>
+    + frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Nonce>
+    + pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+    + subspace_rpc::SubspaceRuntimeApi<Block>
+{
+}
+
+impl<Api> RuntimeApiCollection for Api where
+    Api: BaseRuntimeApiCollection
+        + EthCompatRuntimeApiCollection
+        + sp_consensus_aura::AuraApi<Block, AuraId>
+        + sp_consensus_grandpa::GrandpaApi<Block>
+        + frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Nonce>
+        + pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+        + subspace_rpc::SubspaceRuntimeApi<Block>
+{
+}
 
 /// Only enable the benchmarking host functions when we actually want to benchmark.
 #[cfg(feature = "runtime-benchmarks")]
