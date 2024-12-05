063f03416d674f6fe1bc79e998e26790aa3aabd8
Honza <115138297+Supremesource@users.noreply.github.com>
2024-12-05 13:08:41 -0300

    bump spec_version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -204,7 +204,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("node-subspace"),
     impl_name: create_runtime_str!("node-subspace"),
     authoring_version: 1,
-    spec_version: 512,
+    spec_version: 513,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
